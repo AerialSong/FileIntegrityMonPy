771883b92f914532935aa40f430987d91daae228
AerialSong <93795686+AerialSong@users.noreply.github.com>
2023-10-13 18:36:35 -0600

    Made it so user can choose which directory to monitor; this changes the name of the relevant baseline

--- integrity.py
+++ integrity.py
@@ -7,28 +7,23 @@ import re
 
 BUF_SIZE = 65536 # 64 KB chunks
 
-directory = "/home/kali/Documents/test_files" # Set a directory for file hash scanning. Can be determined by user in main() function
-
 def new_baseline():
     # Calculate hash from the target files and store in baseline.txt
 
-    if os.path.exists("baseline.txt"):
-        os.remove("baseline.txt")
+    if os.path.exists(baseline):
+        os.remove(baseline)
     for filename in os.listdir(directory):
         filehash = hashlib.sha256()
         fn = os.path.join(directory, filename)
         with open(fn, 'rb') as f:
             while True:
                 data = f.read()
-                #print(data)
                 if not data:
                     break
                 if data:
-                    #data = data.encode(encoding = 'UTF-8', errors = 'strict')
                     filehash.update(data)
-                    #filehash(data.encode('utf-8')).update(data)
             f.close()
-        f = open('baseline.txt', 'a')
+        f = open(baseline, 'a')
         print(f"{fn} | {filehash.hexdigest()}", file=f) # Sends the print output to the baseline file
         print(f"{fn} | {filehash.hexdigest()}")
 
@@ -37,14 +32,14 @@ def new_baseline():
 def comp_baseline():
     # Begin (continuously) monitoring files with saved baseline
     base_dict = {}
-    if os.path.exists("baseline.txt"): # Load file|hash from baseline.txt and store them in a dictionary
-        with open("baseline.txt", 'r') as f:
+    if os.path.exists(baseline): # Load file|hash from baseline.txt and store them in a dictionary
+        with open(baseline, 'r') as f:
             for line in f.readlines():
                 fp, filehash = line.split(" | ")
-                re.sub('\n', '', filehash)
-                print(filehash)
+                filehash = filehash.rstrip()
+                print(filehash, '\n')
                 # fp is a file path name
-                # with fp, read each line of baseline.txt
+                # with fp, read each line of the relevant baseline
                 for filename in os.listdir(directory):
                     fpath = os.path.join(directory, filename)
                     # check if the current fp matches any of the files in the directory
@@ -54,9 +49,9 @@ def comp_baseline():
                             test_hash = hashlib.sha256(tf.read())
                             print(test_hash.hexdigest())
                             if test_hash.hexdigest() == filehash:
-                                print(f"{fp!r} has a matching hash.")
+                                print(f"{fp!r} has a matching hash.\n")
                             else:
-                                print(f"{fp!r} has a non-matching hash")
+                                print(f"{fp!r} has a non-matching hash.\n")
 
         # Compare current file: hash to dictionary to check if it's in there
         # if not, it means the file does not exist
@@ -70,6 +65,8 @@ def comp_baseline():
 if __name__ == "__main__":
     print("What would you like to do?\nA) Collect new Baseline?\nB) Begin monitoring files with saved Baseline?\n\n")
     choice = input("Please enter 'A' or 'B': ").upper()
+    directory = input("Please enter the filepath of the directory you would like to monitor: ") # Set a directory for file hash scanning
+    baseline = f'baseline{directory}.txt'.replace('/', '_')
     if choice != 'A' and choice != 'B':
         print("Not a valid option!")
         quit()
