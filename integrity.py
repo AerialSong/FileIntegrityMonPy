1c78915ed4322f188c169874624f4b48cc420a7a
AerialSong <93795686+AerialSong@users.noreply.github.com>
2023-12-13 12:29:54 -0700

    Update integrity.py
    This code isn't finished yet, but I've updated it so that
    - The baseline file is now a JSON file, making for easy access to each key's value
    - When comparing the JSON hashes to the current hashes, it adds the information to a changelog file, and updates the JSON baseline accordingly
    
    Problems:
    - For some reason, every time I use function B, comparing the baseline, it for some reason updates the changelog and the Baseline twice. The first time it updates the hash of the changed file to a hash that doesn't even mean anything, and then it changes it to the actual hash it's been changed to. This is a problem I have yet to solve.

--- integrity.py
+++ integrity.py
@@ -1,6 +1,9 @@
+import json
 import hashlib
 import smtplib
+import json
 import os
+import datetime
 import time
 from email.message import EmailMessage
 import re
@@ -12,11 +15,12 @@ BUF_SIZE = 65536 # 64 KB chunks
 def new_baseline():
     # Calculate hash from the target files and store in baseline.txt
 
-    if os.path.exists(baseline):
-        os.remove(baseline)
+    if os.path.exists(jsfile):
+        os.remove(jsfile)
     # For each directory within the chosen directory
     # scan each file
     # then move onto the next directory and scan each file there until there are no more deeper directories
+    jslist = {}
     for subdir, dirs, files in os.walk(directory):
         for filename in files:
             filehash = hashlib.sha256()
@@ -29,58 +33,74 @@ def new_baseline():
                     if data:
                         filehash.update(data)
                 f.close()
-            f = open(baseline, 'a')
-            print(f"{fn} | {filehash.hexdigest()}", file=f) # Sends the print output to the baseline file
-            print(f"{fn} | {filehash.hexdigest()}")
-
-            f.close()
+                hexdigest = filehash.hexdigest()
+                jslist.update({fn:hexdigest})
+                #print(f"{fn} | {filehash.hexdigest()}", file=f) # Sends the print output to the baseline file
+    with open(jsfile, 'w+') as jf:
+        json.dump(jslist, jf, indent=4)
+    with open(jsfile, 'r') as jf:
+        data = json.load(jf)
+        for i in data:
+            print(i, ':', data[i])
 
 def comp_baseline():
     # Begin (continuously) monitoring files with saved baseline
-    base_dict = {}
-    if os.path.exists(baseline): # Load file|hash from baseline.txt and store them in a dictionary
-        with open(baseline, 'r') as f:
-            for line in f.readlines():
-                fp, filehash = line.split(" | ")
-                filehash = filehash.rstrip()
-                print(filehash, '\n')
-                # fp is a file path name
-                # with fp, read each line of the relevant baseline
-                for subdir, dirs, files in os.walk(directory):
-                    for filename in files:
-                        fpath = os.path.join(subdir, filename)
-                        # check if the current fp matches any of the files in the directory
-                        # if it DOES: start an if statement
-                        if fp == str(fpath):
+    if os.path.exists(jsfile): # Load file|hash from baseline.txt and store them in a dictionary
+        # Exclude the baseline that is currently being read from
+        # Next Step: Instead of repeatedly saying a file has been changed
+        # Say it only once, add that filename and original hash to a log file
+        # Change the baseline to include the hash of this new file
+        while True:
+            for subdir, dirs, files in os.walk(directory):
+                for filename in files:
+                    fpath = os.path.join(subdir, filename)
+                    jf = open(jsfile, 'r')
+                    # Declares the json data as a value
+                    data = json.load(jf)
+                    jf.close()
+                    for i in data:
+                        # Checks if the current file matches the filename in the json data
+                        if i == str(fpath):
                             with open(fpath, 'rb') as tf:
                                 test_hash = hashlib.sha256(tf.read())
-                                print(test_hash.hexdigest())
-                                if test_hash.hexdigest() == filehash:
-                                    print(f"{fp!r} has a matching hash.\n")
+                                # The file hash in the json file is compared to the current hash of the file being scanned
+                                # If the hashes do not match, a log will be updated with the changed log
+                                # and the json file will be updated.
+                                if test_hash.hexdigest() == data[i]:
+                                    pass
                                 else:
-                                    print(f"{fp!r} has a non-matching hash.\n")
-
-        # Compare current file: hash to dictionary to check if it's in there
-        # if not, it means the file does not exist
-        # if the file exists but the hash is different, the file has been compromised     
+                                    print(data[i], '\n')
+                                    print(test_hash.hexdigest())
+                                    print(f"{i!r} has a non-matching hash.\n")
+                                    with open("changelog.txt", 'a') as lf:
+                                        lf.write(f'{i} : {test_hash.hexdigest()}\nFile was changed at {datetime.datetime.now()}\n\n')
+                                    with open(jsfile, 'r+') as jf:
+                                        data[i] = test_hash.hexdigest()
+                                        jf.seek(0)
+                                        jf.write(json.dumps(data))
+                                tf.close()
+                # Consider scanning on an hourly or daily schedule
+                # instead of a continuous real time scan
                     
     else:
         print("Baseline file does not exist in local directory.")
 
 
+try:
+    if __name__ == "__main__":
+        print("What would you like to do?\nA) Collect new Baseline?\nB) Begin monitoring files with saved Baseline?\n\n")
+        choice = input("Please enter 'A' or 'B': ").upper()
+        if choice != 'A' and choice != 'B':
+            print("Not a valid option!")
+            quit()
+        print(f"User has entered: {choice}")
+        directory = input("Please enter the filepath of the directory you would like to monitor: ") # Set a directory for file hash scanning
+        jsfile = f'{directory}.json'.replace('\\', '_').replace('/', '_').replace(':', '_').replace(' ', '_')
+        #baseline = f'baseline_{directory}.txt'.replace('\\', '_').replace('/', '_').replace(':', '_').replace(' ', '_')
 
-if __name__ == "__main__":
-    print("What would you like to do?\nA) Collect new Baseline?\nB) Begin monitoring files with saved Baseline?\n\n")
-    choice = input("Please enter 'A' or 'B': ").upper()
-    if choice != 'A' and choice != 'B':
-        print("Not a valid option!")
-        quit()
-    directory = input("Please enter the filepath of the directory you would like to monitor: ") # Set a directory for file hash scanning
-    baseline = f'baseline{directory}.txt'.replace('/', '_')
-
-    if choice == 'A':
-        new_baseline()
-    elif choice == 'B':
-        comp_baseline()
-
-    print(f"User has entered: {choice}")
+        if choice == 'A':
+            new_baseline()
+        elif choice == 'B':
+            comp_baseline()
+except KeyboardInterrupt:
+    print("\nExiting program...")
